89109b1092b0aa409df625db058ca71dbd93d31b
kokchun <kokchungiang@gmail.com>
2024-08-04 12:58:45 +0200

    dashboard init

--- 03_streamlit_dashboard/dashboard.py
+++ 03_streamlit_dashboard/dashboard.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import pandas as pd 
-
-
+import pandas as pd
 
 
 def layout():
     st.markdown("# First dashboard")
-
+    st.markdown(
+        "The source of the dataset comes from here https://www.kaggle.com/datasets/nitishsharma01/olympics-124-years-datasettill-2020"
+    )
 
 
 if __name__ == "__main__":
