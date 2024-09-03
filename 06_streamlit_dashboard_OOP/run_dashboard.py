0c923cfae17ab8face600bf36e5a6d46bc6b36cb
kokchun <kokchungiang@gmail.com>
2024-09-03 17:16:03 +0200

    fixed run streamlit command

--- 06_streamlit_dashboard_OOP/run_dashboard.py
+++ 06_streamlit_dashboard_OOP/run_dashboard.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 if __name__ == "__main__":
     dashboard_path = Path(__file__).parent / "dashboard.py"
-    subprocess.run(f"streamlit run {dashboard_path}", shell=True)
+    subprocess.run(f"streamlit run '{dashboard_path}'", shell=True)
     
